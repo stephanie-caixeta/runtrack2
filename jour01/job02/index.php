581bde430f61c92b956fe86d57f162b108f6244a
stephanie-caixeta <stephanie.caixeta@laplateforme.io>
2025-07-07 10:55:46 +0200

    job02 ajouté et terminé!

---
+++ jour01/job02/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html lang="fr">
+    <body>
+        <?php
+        /* affiche "Hello LaPlateforme!" 
+        deux fois*/
+        echo "Hello LaPlateforme! <br>";
+        // affiche Hello LaPlateforme pour la deuxième fois
+        echo "Hello LaPlateforme!";
+        ?>
+    </body>
+</html>
