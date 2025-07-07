c02f394b0ab608c0e034bfd1447e70f8acd489eb
stephanie-caixeta <stephanie.caixeta@laplateforme.io>
2025-07-07 13:05:41 +0200

    job02 à jour + job03

---
+++ jour01/job03/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE>
+<html>
+    <body>
+        <?php 
+        $myBool = true;
+        $myNum = 5;
+        $myText = "woo!";
+        $myDec = 10.365;
+        ?>
+
+        <table> 
+            <tr>
+                <th>type</th>
+                <th>nom</th>
+                <th>valeur</th>
+            </tr>
+            <tr>
+                <td>
+                    <?php
+                    echo gettype($myBool);
+                    ?>
+                </td>
+                <td>myBool</td>
+                <td>
+                    <?php
+                    echo $myBool;
+                    ?>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <?php
+                    echo gettype($myNum);
+                    ?>
+                </td>
+                <td>myNum</td>
+                <td>
+                    <?php
+                    echo $myNum;
+                    ?>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <?php
+                    echo gettype($myText);
+                    ?>
+                </td>
+                <td>myText</td>
+                <td>
+                    <?php
+                    echo $myText;
+                    ?>
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <?php
+                    echo gettype($myDec);
+                    ?>
+                </td>
+                <td>myDec</td>
+                <td>
+                    <?php
+                    echo $myDec;
+                    ?>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
