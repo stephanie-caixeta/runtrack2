3f5ccb772e6da981705a2236d3a2e55eedf26ece
stephanie-caixeta <stephanie.caixeta@laplateforme.io>
2025-07-07 10:17:09 +0200

    initialisation du projet

---
+++ jour01/job00/index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="fr">
+<body>
+    <?php 
+    echo "Hello LaPlateforme!";
+    ?>
+</body>
---
+++ jour01/job01/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html lang="fr">
+    <body>
+        <?php 
+            $str = "LaPlateforme";
+            echo $str;
+            $str2 = "Vive";
+            $str3 = "!";
+            echo $str2 + $str + $str3;
+        ?>
+    </body>
+</html>
