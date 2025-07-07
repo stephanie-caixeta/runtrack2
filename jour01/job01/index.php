d5837a64669b7b46771cd58d8b37c9f4a7c0927e
stephanie-caixeta <stephanie.caixeta@laplateforme.io>
2025-07-07 10:46:50 +0200

    job01 mis à jour et terminé

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -3,10 +3,18 @@
     <body>
         <?php 
             $str = "LaPlateforme";
-            echo $str;
+            echo $str . "<br>";
             $str2 = "Vive";
             $str3 = "!";
-            echo $str2 + $str + $str3;
+            echo $str2 . " " . $str . " " . $str3 . "<br>";
+            $val = 6;
+            echo $val . "<br>";
+            $val = $val + 4;
+            echo $val . "<br>";
+            $myBool = true;
+            echo $myBool . "<br>";
+            $myBool = false;
+            echo $myBool . "<br>";
         ?>
     </body>
 </html>
